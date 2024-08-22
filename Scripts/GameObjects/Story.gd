894231e357d78a2f7c414b1dac0e4860b0ffb7d6
Ray <rayfradella@yahoo.com>
2024-08-22 00:55:16 -0400

    Fix console bugs

--- Scripts/GameObjects/Story.gd
+++ Scripts/GameObjects/Story.gd
@@ -3,4 +3,7 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if(Input.is_anything_pressed()==true):
-		get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
+		PlayGame()
+
+func PlayGame():
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
--- Scripts/ResetPlayer.gd
+++ Scripts/ResetPlayer.gd
@@ -2,16 +2,5 @@ extends Area2D
 @export var xResetPos = 0
 @export var yResetPos = 0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 	body.position =Vector2(xResetPos,yResetPos)
